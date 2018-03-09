f24150ea2ada94f902551919e83d4d3a9103be3e
Gontzal <dm3-1-11.17@uni.eus>
2018-03-09 12:24:59 +0100

    usuario klasea gehituta

---
+++ aaaac/proiektua/__init__.py
@@ -0,0 +1,15 @@
+class Artikulo:
+    
+    def __init__(self, Kodea, Izena, Prezioa):
+        
+        self.Kodea=Kodea
+        self.Izena=Izena
+        self.Prezioa=Prezioa
+
+class Usuario:
+       
+    def __init__(self, NAN, Izena, Abizena):
+        
+        self.NAN=NAN
+        self.Izena=Izena
+        self.Abizena=Abizena
