37e017659abca06b6dae17195e7a798c00ca948c
Gontzal <dm3-1-11.17@uni.eus>
2018-03-14 12:49:52 +0100

    artikuluko klaseak gehituta

--- aaaac/proiektua/__init__.py
+++ aaaac/proiektua/__init__.py
@@ -13,3 +13,35 @@ class Usuario:
         self.NAN=NAN
         self.Izena=Izena
         self.Abizena=Abizena
+        
+class Bideo_jokoak(Artikulo):
+    
+    def __init__(self,Generoa, Empresa, plataforma, mult_lokal, mult_online, PEGI, titulua):
+        
+        self.Generoa=Generoa
+        self.Empresa=Empresa
+        self.plataforma=plataforma
+        self.mult_lokal=mult_lokal
+        self.mult_online=mult_online
+        self.PEGI=PEGI
+        self.titulua=titulua
+        
+class Consola(Artikulo):
+    
+    def __init__(self,Plataforma, Empresa):
+        
+        self.Plataforma=Plataforma
+        self.Empresa=Empresa
+        
+class Liburuak(Artikulo):
+    
+    def __init__(self,ISBN,Autor, Generoa,paginas, argitaletxea,argitaratu_eguna, editoriala, titulua):
+        
+        self.ISBN=ISBN
+        self.Autor=Autor
+        self.Generoa=Generoa     
+        self.paginas=paginas
+        self.argitaletxea=argitaletxea
+        self.argitaratu_eguna=argitaratu_eguna
+        self.editoriala=editoriala
+        self.titulua=titulua
